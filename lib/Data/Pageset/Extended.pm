90ad5d25d49415a10191d8e777d9259e803ee6a7
Robert Rothenberg <rrwo@cpan.org>
2019-03-26 10:56:18 +0000

    Add initial version with Data::Page compatability

---
+++ lib/Data/Pageset/Extended.pm
@@ -0,0 +1,154 @@
+package Data::Pageset::Extended;
+
+use Moo;
+
+use List::Util qw/ all min /;
+use PerlX::Maybe;
+use POSIX qw/ ceil floor /;
+use Types::Common::Numeric qw/ PositiveOrZeroInt PositiveInt /;
+use Types::Standard qw/ is_Int Int /;
+
+use namespace::autoclean;
+
+has total_entries => (
+    is      => 'rw',
+    isa     => PositiveOrZeroInt,
+    default => 0,
+);
+
+has entries_per_page => (
+    is      => 'rw',
+    isa     => PositiveInt,
+    default => 10,
+);
+
+has first_page => (
+    is      => 'ro',
+    isa     => Int,
+    default => 1,
+);
+
+has current_page => (
+    is      => 'rw',
+    isa     => Int,
+    lazy    => 1,
+    default => \&first_page,
+    coerce  => sub { floor( $_[0] // 0 ) },
+);
+
+around current_page => sub {
+    my $next = shift;
+    my $self = shift;
+
+    # N.B. unlike Data::Page, setting a value outside the first_page
+    # or last_page will not return that value.
+
+    my $page = $self->$next(@_);
+
+    return $self->first_page if $page < $self->first_page;
+
+    return $self->last_page if $page > $self->last_page;
+
+    return $page;
+};
+
+sub entries_on_this_page {
+    my ($self) = @_;
+
+    if ( $self->total_entries ) {
+        return $self->last - $self->first + 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+sub last_page {
+    my ($self) = @_;
+    return $self->total_entries
+      ? ceil( $self->total_entries / $self->entries_per_page )
+      : $self->first_page;
+}
+
+sub first {
+    my ($self) = @_;
+    if ( $self->total_entries ) {
+        return ( $self->current_page - 1 ) * $self->entries_per_page + 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+sub last {
+    my ($self) = @_;
+    if ( $self->current_page == $self->last_page ) {
+        return $self->total_entries;
+    }
+    else {
+        return $self->current_page * $self->entries_per_page;
+    }
+}
+
+sub previous_page {
+    my ($self) = @_;
+    my $page = $self->current_page;
+
+    return $page > $self->first_page
+      ? $page - 1
+      : undef;
+}
+
+sub next_page {
+    my ($self) = @_;
+    my $page = $self->current_page;
+
+    return $page < $self->last_page
+      ? $page + 1
+      : undef;
+}
+
+sub splice {
+    my ( $self, $items ) = @_;
+
+    my $last = min( $self->last, scalar(@$items) );
+
+    return $last
+      ? @{$items}[ $self->first - 1 .. $last - 1 ]
+      : ();
+}
+
+sub skipped {
+    my ($self) = @_;
+    return $self->total_entries
+        ? $self->first - 1
+        : 0;
+}
+
+sub change_entries_per_page {
+    my ( $self, $value ) = @_;
+
+    my $first = $self->first;
+
+    $self->entries_per_page($value);
+
+    return $self->current_page( $self->first_page + $first / $value );
+}
+
+sub BUILDARGS {
+    my ( $class, @args ) = @_;
+
+    if ( @args && ( @args <= 3 ) && all { is_Int($_) } @args ) {
+
+        return {
+                  total_entries    => $args[0],
+            maybe entries_per_page => $args[1],
+            maybe current_page     => $args[2],
+        };
+
+    }
+
+    return {@args};
+}
+
+1;

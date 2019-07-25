7e2a8d9184e1072f68f177b914e5268777bbfd5b
Robert Rothenberg <rrwo@cpan.org>
2019-07-25 13:01:16 +0100

    Bump VERSION to v0.3.0

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Data::Pageset::Exponential - Page numbering for very large page numbers
 
 # VERSION
 
-version v0.2.2
+version v0.3.0
 
 # SYNOPSIS
 
--- lib/Data/Pageset/Exponential.pm
+++ lib/Data/Pageset/Exponential.pm
@@ -18,7 +18,7 @@ use namespace::autoclean;
 # RECOMMEND PREREQ: Type::Tiny::XS
 # RECOMMEND PREREQ: Ref::Util::XS
 
-our $VERSION = 'v0.2.3';
+our $VERSION = 'v0.3.0';
 
 =head1 SYNOPSIS
 
